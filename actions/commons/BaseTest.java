b5c7e01670fcf55716f22997c7a1996656df0e2e
LukePhan <ngoclu91@gmail.com>
2022-11-27 16:07:59 +0700

    [add] create multiple browsers test
    [add] add WebDriverManager handle

--- actions/commons/BaseTest.java
+++ actions/commons/BaseTest.java
@@ -4,32 +4,36 @@ import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.opera.OperaDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseTest {
 	WebDriver driver;
 	String projectPath = System.getProperty("user.dir");
+	String osName = System.getProperty("os.name");
 	
 	public WebDriver getBrowserDriver(String browserName) {
-		switch(browserName)	{
-		case "Firefox":
-			System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver");
-			driver = new FirefoxDriver();
+		BrowserName browser = BrowserName.valueOf(browserName.toUpperCase());
+		switch(browser)	{
+		case FIREFOX:
+			driver = WebDriverManager.firefoxdriver().create();
+			//System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver");
+			//driver = new FirefoxDriver();
 			break;
-		case "Chrome":
-			System.setProperty("webdriver.chrome.driver", projectPath + "/browserDrivers/chromedriver");
-			driver = new ChromeDriver();
+		case CHROME:
+			driver = WebDriverManager.chromedriver().create();
+			//System.setProperty("webdriver.chrome.driver", projectPath + "/browserDrivers/chromedriver");
+			//driver = new ChromeDriver();
 			break;
-		case "Edge":
-			System.setProperty("webdriver.edge.driver", projectPath + "/browserDrivers/msedgedriver");
-			driver = new EdgeDriver();
+		case EDGE:
+			driver = WebDriverManager.edgedriver().create();
+			//System.setProperty("webdriver.edge.driver", projectPath + "/browserDrivers/msedgedriver");
+			//driver = new EdgeDriver();
 			break;
-		case "Opera":
-			System.setProperty("webdriver.opera.driver", projectPath + "/browserDrivers/operadriver");
-			driver = new OperaDriver();
+		case OPERA:
+			driver = WebDriverManager.operadriver().create();
+			//System.setProperty("webdriver.opera.driver", projectPath + "/browserDrivers/operadriver");
+			//driver = new OperaDriver();
 			break;
 		default:
 			throw new RuntimeException("Cannot find browser name.");

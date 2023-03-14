c3a873e9561989dbd180fe3b5f21aba298bb68c1
LukePhan <ngoclu91@gmail.com>
2023-03-14 12:13:05 +0700

    [add] add more Global Constant

--- actions/commons/GlobalConstant.java
+++ actions/commons/GlobalConstant.java
@@ -1,5 +1,26 @@
 package commons;
 
+import java.io.File;
+
 public class GlobalConstant {
+	public static final String PORTAL_TEST_URL = "https://demo.nopcommerce.com/";
+	public static final String ADMIN_TEST_URL = "https://admin-demo.nopcommerce.com/";
+	public static final String PROJECT_PATH = System.getProperty("user.dir");
+	public static final String OS_NAME = System.getProperty("os.name");
+	public static final String UPLOAD_FILE = PROJECT_PATH + File.separator + "uploadFiles";
+	public static final String DOWNLOAD_FILE = PROJECT_PATH + File.separator + "downloadFiles";
+	public static final String BROWSER_LOG = PROJECT_PATH + File.separator + "browserLogs";
+	public static final String DRAG_DROP_HTML5 = PROJECT_PATH + File.separator + "dragDropHTML5";	
 	
+	public static final String DB_DEV_URL = "";
+	public static final String DB_DEV_USER = "";
+	public static final String DB_DEV_PASS = "";
+	
+	public static final String DB_TEST_URL = "";
+	public static final String DB_TEST_USER = "";
+	public static final String DB_TEST_PASS = "";
+	
+	public static final long SHORT_TIMEOUT = 5;
+	public static final long LONG_TIMEOUT = 10;
+	public static final long RETRY_TEST_FAIL = 3;
 }

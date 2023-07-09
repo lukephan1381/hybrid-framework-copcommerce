bb5adc125e9d46fb291d67f67fde5b7a775c7297
LukePhan <ngoclu91@gmail.com>
2023-07-09 10:23:47 +0700

    [add] Level 1 share data by class variable

---
+++ testcases/com/nopcommerce/common/Common_01_Register_New_Account.java
@@ -0,0 +1,106 @@
+package com.nopcommerce.common;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+
+import commons.BaseTest;
+import pageObjects.nopcommerce.PageGeneratorManager;
+import pageObjects.nopcommerce.UserCustomerInfoPageObject;
+import pageObjects.nopcommerce.UserHomePageObject;
+import pageObjects.nopcommerce.UserLoginPageObject;
+import pageObjects.nopcommerce.UserOrdersPageObject;
+import pageObjects.nopcommerce.UserProductReviewPageObject;
+import pageObjects.nopcommerce.UserRegisterPageObject;
+import pageObjects.nopcommerce.UserRewardPageObject;
+import pageObjects.nopcommerce.UserSideBarPageObject;
+
+public class Common_01_Register_New_Account extends BaseTest{
+	
+	WebDriver driver;
+	UserHomePageObject homePage;
+	UserRegisterPageObject registerPage;
+	UserLoginPageObject loginPage;
+	UserCustomerInfoPageObject customerInfoPage;
+	UserOrdersPageObject orderPage;
+	UserRewardPageObject rewardPage;
+	UserProductReviewPageObject productReviewPage;
+	UserSideBarPageObject userSideBar;
+	//String osName = System.getProperty("os.name");
+	public static String emailAddress,password;
+	String firstName,lastName;
+	
+	
+	@Parameters({"browser","userURL"})
+	@BeforeTest(description = "Create new common user for all Classes Test")
+	public void Register(String browserName, String userURL) {
+		
+		driver = getBrowserDriver(browserName,userURL);
+		homePage = PageGeneratorManager.getUserHomePage(driver);
+		
+		firstName = "Luke";
+		lastName = "Phan";
+		emailAddress = "lukephan" + generateRandomNumber() + "@auto.vn";
+		password = "qqqq1111";
+
+		//Click on Register button
+		log.info("Register - Step 01: Navigate to REGISTER page");
+		registerPage = homePage.clickToRegisterButton();
+		
+		//Select gender
+		log.info("Register - Step 02: Click on Male radio button");
+		registerPage.clickToGenderMaleRadio();
+		
+		//Input first name & last name
+		log.info("Register - Step 03: Input to firstname textbox");
+		registerPage.inputToFirstNameTextbox(firstName);
+		log.info("Register - Step 04: Input to lastname textbox");
+		registerPage.inputToLastNameTextbox(lastName);
+		
+		//Select birthday
+		log.info("Register - Step 05: Select birthday day");
+		registerPage.selectDayDropdown("28");
+		log.info("Register - Step 06: Select birthday month");
+		registerPage.selectMonthDropdown("January");
+		log.info("Register - Step 07: Select birthday year");
+		registerPage.selectYearDropdown("1991");
+		
+		//Input email address
+		log.info("Register - Step 08: Input to email address textbox");
+		registerPage.inputToEmailTextbox(emailAddress);
+		
+		//Input & confirm password
+		log.info("Register - Step 09: Input to company textbox");
+		registerPage.inputToCompanyTextbox("Marvel");
+		log.info("Register - Step 10: Input to password textbox");
+		registerPage.inputToPasswordTextbox(password);
+		log.info("Register - Step 11: Input to confirm password textbox");
+		registerPage.inputToConfirmPasswordTextbox(password);
+		
+		//Click REGISTER button
+		log.info("Register - Step 12: Click to REGISTER button");
+		registerPage.clickToRegisterButton();
+		
+		//Verify success message
+		log.info("Register - Step 13: Verify if register completed");
+		Assert.assertEquals(registerPage.getRegisterResultMessage(), "Your registration completed");
+		
+		//logout of current account
+		log.info("Register - Step 14: Return to HOME page");
+		//registerPage.clickToContinueButton();
+		
+		homePage = registerPage.clickToContinueButton();
+		log.info("Register - Step 15: Return to HOME page successfully");
+		Assert.assertEquals(homePage.getHomePageURL(), "https://demo.nopcommerce.com/");
+	}
+	
+	@AfterTest
+	public void afterClass() {
+		driver.quit();
+	}
+	
+
+	
+}

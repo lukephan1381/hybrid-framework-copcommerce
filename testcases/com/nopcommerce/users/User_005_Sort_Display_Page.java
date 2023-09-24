7492ebb7f285a3f71b582633243834ab9a385190
LukePhan <ngoclu91@gmail.com>
2023-09-24 20:12:57 +0700

    [add] Sort TC1 and TC2

--- testcases/com/nopcommerce/users/User_005_Sort_Display_Page.java
+++ testcases/com/nopcommerce/users/User_005_Sort_Display_Page.java
@@ -1,10 +1,17 @@
 package com.nopcommerce.users;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+
+import com.google.common.collect.Comparators;
 
 import commons.BaseTest;
 import pageObjects.nopcommerce.PageGeneratorManager;
@@ -28,6 +35,25 @@ public class User_005_Sort_Display_Page extends BaseTest{
 		productPage.selectSortTypeByText("Name: A to Z");
 		productPage.getListOfProducts();
 		
+		List<String> listOfProduct = new ArrayList<String>();
+		for (int i = 0; i < productPage.getListOfProducts().size(); i++) {
+			listOfProduct.add(productPage.getListOfProducts().get(i).getText());
+		}
+
+		Assert.assertTrue(Comparators.isInOrder(listOfProduct, Comparator.naturalOrder()));
+	}
+	
+	@Test
+	public void TC_002_Sort_Z_To_A() {
+		productPage.selectSortTypeByText("Name: Z to A");
+		productPage.getListOfProducts();
+		
+		List<String> listOfProduct = new ArrayList<String>();
+		for (int i = 0; i < productPage.getListOfProducts().size(); i++) {
+			listOfProduct.add(productPage.getListOfProducts().get(i).getText());
+		}
+
+		Assert.assertTrue(Comparators.isInOrder(listOfProduct, Comparator.reverseOrder()));
 	}
 	
 	@AfterClass()
